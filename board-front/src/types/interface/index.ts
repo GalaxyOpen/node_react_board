7124372fbc972e6b4cf43d3676bd03a02e0488b8
DA317 <keehong11@naver.com>
2024-11-01 15:24:56 +0900

    241101 board_back 1. BoardListitem.java 파일 추가 2. FavoriteListItem.java 파일 추가 3. CommentListItem.java 파일 추가
    board_front
    1. board-list-item.interface.ts 파일 추가
    2. interface 폴더 안에 index.ts 파일 추가
    3. favorite-list-item.interface.ts 파일 추가
    4. comment-list-item.interface.ts 파일 추가

---
+++ board-front/src/types/interface/index.ts
@@ -0,0 +1,9 @@
+import BoardListItem from './board-list-item.interface';
+import FavoriteListItem from './favorite-list-item.inferface';
+import CommentListItem from './comment-list-item.interface';
+
+export type {
+    BoardListItem,
+    FavoriteListItem,
+    CommentListItem
+}
