1eb1cd99ddc1708592ce828f302c047186543e1a
DA317 <keehong11@naver.com>
2024-11-04 10:41:43 +0900

    24.11.04 1시간 1. index.tsx 변경 2. style.css -> board-list-item-write-box 부터 추가 3. app.css 내용 삭제 4. index.css 내용 변경 -> 눈누 프리텐다드 검색하여 붙여넣기 5. assets/example_image_human.jpg 추가

--- board-front/src/components/BoardListItem/index.tsx
+++ board-front/src/components/BoardListItem/index.tsx
@@ -1,36 +1,62 @@
 import React from 'react';
 import './style.css';
+import { BoardListItem } from 'types/interface';
+import { useNavigate } from 'react-router-dom';
+
+interface Props {
+  boardListItem : BoardListItem
+}
 
 // component : Board List Item 컴포넌트  // 
-export default function BoardListItem() {
+export default function BoardListItem({boardListItem}:Props) {
+
+  // Properties                               //
+
+  const { boardNumber, title, content, boardTitleImage } = boardListItem;
+  const { favoriteCount, commentCount, viewCount } = boardListItem;
+  const { writeDatetime, writerNicknmae, writerProfileImage  } = boardListItem;
+
+  // function: 네비게이트 함수     // 
+
+  const navigator = useNavigate();
+
+
+  // event handler: 게시물 아이템 클릭 이벤트 처리 함수                //
+  const onClickHandler= () => {
+    navigator(boardNumber);
+  }
+
 
   // render : Board List Item 컴포넌트 렌더링  // 
 
   return (
-    <div className='board-list-item'>
+    <div className='board-list-item' onClick={onClickHandler}>
       <div className='board-list-item-main-box'>
         <div className='board-list-item-top'>
           <div className='board-list-item-profile-box'>
-            <div className='board-list-item-profile-image' style={{backgroundImage: 'url(https://i.namu.wiki/i/Ht7Gwt4A7gckA36YjEd28fHuAejxMBus82jI8Q37VkbKcpYeWdgtWq0uHfmwzysnN5qIBu-BpO6BrNpjdYhBouSV5sPbodwvNKz-F7Won-RUJr3JIpAC8RD_znjiyQnNyzazrP3aVE5pZyTEM7OaOg.webp)'}}></div>
+            <div className='board-list-item-profile-image' style={{backgroundImage: 'url(%{writerProfileImage})'}}></div>
           </div>
           <div className='board-list-item-write-box'>
-            <div className='board-list-item-nickname'>{'The fortress 2 Blue World'}</div>
-            <div className='board-list-item-write-datetime'>{'2024.11.04'}</div>
+            <div className='board-list-item-nickname'>{writerNicknmae}</div>
+            <div className='board-list-item-write-date'>{writeDatetime}</div>
           </div>
         </div>
         <div className='board-list-item-middle'>
-          <div className='board-list-item-title'>{'wellcome to e-shibal hell'}</div>
-          <div className='board-list-item-content'></div>
+          <div className='board-list-item-title'>{title}</div>
+          <div className='board-list-item-content'>{content}</div>
         </div>
         <div className='board-list-item-bottom'>
           <div className='board-list-item-counts'>
-            {`댓글 0 · 좋아요 0 · 조회수 0`}
+            {`댓글 ${commentCount} · 좋아요 ${favoriteCount} · 조회수 ${viewCount}`}
           </div>
         </div>
       </div>
-      <div className='board-list-item-image'>
-        <div className='board-list-item-image' style={{backgroundImage:'url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.cafe.daum.net%2Frocksoccer%2FADs1%2F126917%3FlistURI%3D%252Frocksoccer%252FADs1&psig=AOvVaw2m1wvc7f9NxLXvrA3CLA2B&ust=1730764928734000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLDPjvivwYkDFQAAAAAdAAAAABAI)'}}></div>
-      </div>
+      {boardTitleImage ! == null &&(
+          <div className='board-list-item-image-box'>
+          <div className='board-list-item-image' style={{backgroundImage:'url(${boardTitleImage})'}}></div>
+          </div>
+      )}
+
     </div>
   )
 }
