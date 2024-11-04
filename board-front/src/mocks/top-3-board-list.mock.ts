0da43960be8e01f777d2382f3cd2775457c558ad
DA317 <keehong11@naver.com>
2024-11-04 15:55:01 +0900

    24.11.04     overflow: hidden;     text-overflow: ellipsis;     white-space: nowrap; 적용 되는지 체크 확인 완료.

--- board-front/src/mocks/top-3-board-list.mock.ts
+++ board-front/src/mocks/top-3-board-list.mock.ts
@@ -3,8 +3,8 @@ import { BoardListItem } from 'types/interface';
 const top3BoardListMock: BoardListItem[] = [
     {
 		"boardNumber": 1,
-		"title": "React Project 시작",
-		"content": "쓸 게 너무 많은데 리뷰할 때 괜찮을까...",
+		"title": "Whiskey & Morphine",
+		"content": "Jave와 Python 둘 중 하나는 분명한 Whiskey이지만, 하나는 Whiskey일 수도, Morphine일 수도 있다.",
 		"boardTitleImage": 'https://t4.ftcdn.net/jpg/04/39/45/47/240_F_439454712_84RhEpQVA1FArMRyVh2igGFmTegNQSFP.jpg',
         "favoriteCount": 0,
         "commentCount": 0,
