c3167025e3d9b6a2efeb92f5ae3ddd11a3c0c9f3
DA317 <keehong11@naver.com>
2024-11-19 15:55:03 +0900

    24.11.19 Auth 페이지 로그인 카드 A InputBox >> index.tsx 에서 구문 추가     //   event handler: input 값 변경 이벤트 처리 함수     //     const onChangeHandler = (event:ChangeEvent<HTMLInputElement>) =>{         const value = event.target.value;         // 이 구문과 같음 const { value } = event.target;         setValue(value); // << 이 구문 추가     }

--- board-front/src/components/InputBox/index.tsx
+++ board-front/src/components/InputBox/index.tsx
@@ -12,7 +12,7 @@ interface Props{
     error : boolean;
 
     // 변수 뒤에 ?를 붙이면 필수가 아니라는 의미
-    icon? : string;
+    icon? : 'eye-light-off-icon' | 'eye-light-on-icon' |'expand-right-light-icon' ;
     onButtonClick?: () => void;
 
     message?: string;
@@ -20,7 +20,7 @@ interface Props{
     onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
 }
     //          component : Input Box 컴포넌트          //
-const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref)=> {
+    const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref)=> {
 
     //          state : properties         // 
     const {label, type, placeholder, value ,error, icon, message} =props;
@@ -30,6 +30,7 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref)=> {
     const onChangeHandler = (event:ChangeEvent<HTMLInputElement>) =>{
         const value = event.target.value;
         // 이 구문과 같음 const { value } = event.target;
+        setValue(value); // << 이 구문 추가(24.11.19)
     }
 
     //   event handler: input 키 이벤트 처리 함수     // 
@@ -45,7 +46,7 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref)=> {
             <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
                 <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler} />
                 {onButtonClick !== undefined &&
-                    <div className='icon-button'>
+                    <div className='icon-button' onClick={onButtonClick}>
                         { icon !== undefined && (<div className={`icon ${icon}`}></ div>)}
                 </div>
 
