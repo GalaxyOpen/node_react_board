1392851afbe8db55d9aa1e39cca704b97398a465
DA317 <keehong11@naver.com>
2024-11-28 23:12:22 +0900

    24.11.28 PostBoardResponseDTO 생성

---
+++ board-back/src/main/java/com/lkh/board_back/dto/response/board/PostBoardResponseDTO.java
@@ -0,0 +1,28 @@
+package com.lkh.board_back.dto.response.board;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.lkh.board_back.common.ResponseCode;
+import com.lkh.board_back.common.ResponseMessage;
+import com.lkh.board_back.dto.response.ResponseDTO;
+
+import lombok.Getter;
+@Getter
+public class PostBoardResponseDTO extends ResponseDTO {
+    
+    private PostBoardResponseDTO(){
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+
+    }
+
+    public static ResponseEntity<PostBoardResponseDTO> success() {
+        PostBoardResponseDTO result = new PostBoardResponseDTO();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDTO> notExistUser(){
+        ResponseDTO result = new ResponseDTO(ResponseCode.NOT_EXIST_USER, ResponseMessage.NOT_EXIST_USER);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+    }
+}
