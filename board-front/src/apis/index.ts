7577d7df966b01d432db6c4dea8b6bdc8890a422
DA317 <keehong11@naver.com>
2024-12-02 16:48:03 +0900

    24.12.02 게시물 작성 페이지 API 연동 BackEnd 먼저 실행하고 시작 apis > index.ts

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -3,6 +3,8 @@ import { SignInRequestDTO, SignUpRequestDTO } from './request/auth';
 import { SignInResponseDTO, SignUpResponseDTO } from './response/auth';
 import { ResponseDTO } from './response';
 import { GetSignInUserResponseDTO } from './response/user';
+import { PostBoardRequestDTO } from './request/board';
+import { PostBoardResponseDTO } from './response/board';
 
 const DOMAIN = 'http://localhost:4000';
 
@@ -28,6 +30,7 @@ export const signInRequest = async (requestBody: SignInRequestDTO) => {
         })
     return result;    
 }
+
 export const signUpRequest = async (requestBody: SignUpRequestDTO) =>{
     const result = await axios.post(SIGN_UP_URL(), requestBody)
         .then(response =>{
@@ -42,6 +45,22 @@ export const signUpRequest = async (requestBody: SignUpRequestDTO) =>{
     return result
 }
 
+const POST_BOARD_URL = () =>`${API_DOMAIN}/board`;
+
+export const postBoardRequest = async (requestBody: PostBoardRequestDTO, accessToken:string)=>{
+    const result = await axios.post(POST_BOARD_URL(), requestBody, authorization(accessToken))
+        .then(response =>{
+            const responseBody: PostBoardResponseDTO = response.data;
+            return responseBody;
+        })
+        .catch(error =>{
+            if (!error.response) return null;
+            const responseBody : ResponseDTO = error.response.data;
+            return responseBody;
+        })
+    return result;    
+}
+
 const GET_SIGN_IN_USER_URL = () => `${API_DOMAIN}/user`;
 
 export const getSignInUserRequest = async (accessToken: string) =>{
@@ -54,6 +73,23 @@ export const getSignInUserRequest = async (accessToken: string) =>{
             if (!error.response) return null;
             const responseBody: ResponseDTO =error.response.data;
             return responseBody;
+        })
+    return result;    
+}
+const FILE_DOMAIN = `${DOMAIN}/file`;
+
+const FILE_UPLOAD_URL = () => `${FILE_DOMAIN}/upload`;
+
+const multipartFormData = {headers:{'Content-Type': 'multipart/form-data'}}
+
+export const fileUploadRequest = async (data:FormData) =>{
+    const result = await axios.post(FILE_UPLOAD_URL(), data, multipartFormData)
+        .then(response =>{
+            const responseBody: string = response.data;
+            return responseBody;
+        })
+        .catch(error =>{
+            return null;
         })
     return result;    
 }
