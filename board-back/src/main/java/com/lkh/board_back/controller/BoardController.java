883c8ba2888e5bb64516f7e70b675a68f376c81a
DA317 <keehong11@naver.com>
2024-12-03 12:25:20 +0900

    24.12.03 특정 게시물 불러오기 API 구현 상단 : 게시물 상세 내용 중단 : 좋아요 갯수와 리스트 하단 : 댓글과 댓글 리스트
    BoardController.java : 경로와 GetBoard ( )메소드 구현.

--- board-back/src/main/java/com/lkh/board_back/controller/BoardController.java
+++ board-back/src/main/java/com/lkh/board_back/controller/BoardController.java
@@ -4,12 +4,15 @@ import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lkh.board_back.dto.request.board.PostBoardRequestDTO;
+import com.lkh.board_back.dto.response.board.GetBoardResponseDTO;
 import com.lkh.board_back.dto.response.board.PostBoardResponseDTO;
 import com.lkh.board_back.service.BoardService;
 
@@ -21,6 +24,12 @@ import lombok.RequiredArgsConstructor;
 public class BoardController {
 
     private final BoardService boardService;
+
+    @GetMapping("/{boardNumber}")
+    public ResponseEntity<? super GetBoardResponseDTO> getBoard(@PathVariable("boardNumber") Integer boardNumber){
+        ResponseEntity<? super GetBoardResponseDTO> response = boardService.getBoard(boardNumber);
+        return response;
+    }
 
     @PostMapping("")
     public ResponseEntity<? super PostBoardResponseDTO> postBoard(
