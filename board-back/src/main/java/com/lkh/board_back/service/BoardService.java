e51a0dc43818b28221755880ef5d602442f86d31
DA317 <keehong11@naver.com>
2024-12-03 11:37:51 +0900

    24.12.03 특정 게시물 불러오기 API 구현 상단 : 게시물 상세 내용 중단 : 좋아요 갯수와 리스트 하단 : 댓글과 댓글 리스트 - BoardService.java : GetBoard 로직 추가

--- board-back/src/main/java/com/lkh/board_back/service/BoardService.java
+++ board-back/src/main/java/com/lkh/board_back/service/BoardService.java
@@ -3,9 +3,11 @@ package com.lkh.board_back.service;
 import org.springframework.http.ResponseEntity;
 
 import com.lkh.board_back.dto.request.board.PostBoardRequestDTO;
+import com.lkh.board_back.dto.response.board.GetBoardResponseDTO;
 import com.lkh.board_back.dto.response.board.PostBoardResponseDTO;
 
 public interface BoardService {
+    ResponseEntity<? super GetBoardResponseDTO> getBoard(Integer boardNumber); // 게시물 상세의 URL을 확인해보면 pathvariable로 boardNumber를 받아오고 있기 때문. 
     ResponseEntity<? super PostBoardResponseDTO> postBoard(PostBoardRequestDTO DTO, String email);
 
     
