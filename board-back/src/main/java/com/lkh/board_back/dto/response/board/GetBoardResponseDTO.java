3eb65fbbc8761ad26d2475e65424908999ab75f9
DA317 <keehong11@naver.com>
2024-12-03 12:26:21 +0900

    24.12.03 특정 게시물 불러오기 API 구현 상단 : 게시물 상세 내용 중단 : 좋아요 갯수와 리스트 하단 : 댓글과 댓글 리스트
    GetBoardResponseDTO.java : GetBoard( ) 메소드를 통해 가지고 올 목록과 success, error에 관련된 코드 작성

--- board-back/src/main/java/com/lkh/board_back/dto/response/board/GetBoardResponseDTO.java
+++ board-back/src/main/java/com/lkh/board_back/dto/response/board/GetBoardResponseDTO.java
@@ -1,5 +1,6 @@
 package com.lkh.board_back.dto.response.board;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import com.lkh.board_back.common.ResponseCode;
 import com.lkh.board_back.common.ResponseMessage;
 import com.lkh.board_back.dto.response.ResponseDTO;
+import com.lkh.board_back.entity.ImageEntity;
+import com.lkh.board_back.repository.resultSet.GetBoardResultSet;
 
 import lombok.Getter;
 
@@ -23,12 +26,27 @@ public class GetBoardResponseDTO extends ResponseDTO {
     private String writerNickname;
     private String writerProifileImage;
     // success
-    private GetBoardResponseDTO() {
+    private GetBoardResponseDTO(GetBoardResultSet resultSet, List<ImageEntity> imageEntities) {
         super(ResponseCode.SUCCESS,  ResponseMessage.SUCCESS);
+
+        List<String> boardImageList = new ArrayList<>();
+        for (ImageEntity  imageEntity:imageEntities){
+            String boardImage = imageEntity.getImage();
+            boardImageList.add(boardImage);
+        }
+
+        this.boardNumber = resultSet.getBoardNumber();
+        this.title =resultSet.getTitle();
+        this.content=resultSet.getContent();
+        this.boardImageList = boardImageList;
+        this.writeDateTime = resultSet.getWriteDateTime();
+        this.writerEmail = resultSet.getWriterEmail();
+        this.writerNickname = resultSet.getWriterNickname();
+        this.writerProifileImage = resultSet.getWriterProfileImage();
     }
 
-    public static ResponseEntity<GetBoardResponseDTO> success() {
-        GetBoardResponseDTO result = new GetBoardResponseDTO();
+    public static ResponseEntity<GetBoardResponseDTO> success(GetBoardResultSet resultSet, List<ImageEntity> imageEntities) {
+        GetBoardResponseDTO result = new GetBoardResponseDTO(resultSet, imageEntities);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
     // error 
