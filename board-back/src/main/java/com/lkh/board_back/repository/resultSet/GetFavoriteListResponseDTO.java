88832624eb2e9cb249f1364ca64ad7e90a3f47a3
DA317 <keehong11@naver.com>
2024-12-04 16:06:54 +0900

    24.12.04 게시물 좋아요 리스트 불러오기 API 구현 URL : /api/v1/board/{boardNumber}/favorite-list GetFavoriteListResponseDTO.java 생성

---
+++ board-back/src/main/java/com/lkh/board_back/repository/resultSet/GetFavoriteListResponseDTO.java
@@ -0,0 +1,7 @@
+package com.lkh.board_back.repository.resultSet; 
+
+public interface GetFavoriteListResponseDTO {
+    String getEmail();
+    String getNickname();
+    String getProfileImage();
+}
