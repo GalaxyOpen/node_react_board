920ece1132766d2cc6f388655f200317161ca15e
DA317 <keehong11@naver.com>
2024-12-05 15:54:58 +0900

    24.12.05 게시물 댓글 리스트 불러오기 API 구현 URL : GET /api/v1/board/{boardNumber}/comment-list CommentRepository.java : 쿼리문을 통해 repository 내에 코드 작성.

--- board-back/src/main/java/com/lkh/board_back/repository/CommentRepository.java
+++ board-back/src/main/java/com/lkh/board_back/repository/CommentRepository.java
@@ -1,11 +1,30 @@
 package com.lkh.board_back.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lkh.board_back.entity.CommentEntity;
+import com.lkh.board_back.repository.resultSet.GetCommentListResultSet;
 
 @Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
-    
+
+    @Query(
+        value = 
+        "SELECT " +
+            "U.nickname AS nickname, " +
+            "U.profile_image AS profile_image, " +
+            "C.write_datetime as write_datetime, " +
+            "C.content AS content " +
+        "FROM comment AS C " +
+        "INNER JOIN user AS U " +
+        "on C.user_email = U.email " +
+        "WHERE C.board_number = ?1 " +
+        "ORDER BY write_datetime DESC; ",
+        nativeQuery = true
+    )
+    List<GetCommentListResultSet> getCommentList(Integer boardNumber);
 }
