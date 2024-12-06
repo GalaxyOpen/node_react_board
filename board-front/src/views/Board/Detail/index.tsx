334ba03b6347bd54233f234c209e8cd021c0efda
DA317 <keehong11@naver.com>
2024-12-06 11:57:58 +0900

    24.12.06 게시물 상세보기 상단 스타일 작성 views > Board > Detail > index.tsx, style.css 변경

--- board-front/src/views/Board/Detail/index.tsx
+++ board-front/src/views/Board/Detail/index.tsx
@@ -6,11 +6,21 @@ import { CommentListMock, favoriteListMock } from 'mocks';
 import CommentItem from 'components/CommentItem';
 import Pagination from 'components/Pagination';
 
+import defaultProfileImage from 'assets/image/default-profile-image.png';
+
 //       component: 게시물 상세 화면 컴포넌트           //
 export default function BoardDetail() {
   
   //       component: 게시물 상세 상단 컴포넌트           //
   const BoardDetailTop = () =>{
+
+    //        state : more 버튼 상태        //
+    const [showMore, setShowMore] = useState<Boolean>(false);
+
+    //        event handler : more 버튼 클릭 이벤트 처리        //
+    const onMoreButtonClickHandler = () =>{
+      setShowMore(!showMore);
+    }
 
     //        render : 게시물 상세 화면 컴포넌트 렌더링      //
     return (
@@ -19,25 +29,27 @@ export default function BoardDetail() {
           <div className='board-detail-title'>{'동해물과 백두산이 마르고 닳도록'}</div>
           <div className='board-detail-top-sub-box'>
             <div className='board-detail-write-info-box'>
-              <div className='board-detail-writer-profile-image'></div>
+              <div className='board-detail-writer-profile-image' style={{backgroundImage: `url(${defaultProfileImage})`}}></div>
               <div className='board-detail-writer-nickname'>{'자바맨'}</div>
               <div className='board-detail-info-divider'>{'\|'}</div>
               <div className='board-detail-writer-date'>{'2024.12.06'}</div>
             </div>
-            <div className='icon-button'>
+            <div className='icon-button' onClick={onMoreButtonClickHandler}>
               <div className='icon more-icon'></div>
             </div>
+            {showMore && 
             <div className='board-detail-more-box'>
               <div className='board-detail-update-button'>{'수정'}</div>
               <div className='divider'></div>
               <div className='board-detail-delete-button'>{'삭제'}</div>
             </div>
+            }
           </div>
         </div>
         <div className='divider'></div>
         <div className='board-detail-top-main'>
           <div className='board-detail-main-text'>{'되는 것인가'}</div>
-          <div className='board-detail-main-image'></div>
+          <img className='board-detail-main-image' src='https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/6Na/image/aySupoB9n6H2vpBLC3KMfVZ2VCY.jpg' />
         </div>
       </div>
     );
