a828a4ea2e21d9855dd9b5e68fa63baf8ec64e1b
DA317 <keehong11@naver.com>
2024-12-06 10:09:20 +0900

    24.12.06 게시물 상세보기 상단 컴포넌트 구조 작성 views > board > detail > index.tsx 코드 큰 틀만 작성

--- board-front/src/views/Board/Detail/index.tsx
+++ board-front/src/views/Board/Detail/index.tsx
@@ -4,8 +4,55 @@ import './style.css'
 //       component: 게시물 상세 화면 컴포넌트           //
 export default function BoardDetail() {
   
+  //       component: 게시물 상세 상단 컴포넌트           //
+  const BoardDetailTop = () =>{
+
+    //        render : 게시물 상세 화면 컴포넌트 렌더링      //
+    return (
+      <div id='board-detail-top'>
+        <div className='board-detail-top-header'>
+          <div className='board-detail-title'>{'동해물과 백두산이 마르고 닳도록'}</div>
+          <div className='board-detail-top-sub-box'>
+            <div className='board-detail-write-info-box'>
+              <div className='board-detail-writer-profile-image'></div>
+              <div className='board-detail-writer-nickname'>{'자바맨'}</div>
+              <div className='board-detail-info-divider'>{'\|'}</div>
+              <div className='board-detail-writer-date'>{'2024.12.06'}</div>
+            </div>
+            <div className='icon-button'>
+              <div className='icon more-icon'></div>
+            </div>
+            <div className='board-detail-more-box'>
+              <div className='board-detail-update-button'>{'수정'}</div>
+              <div className='divider'></div>
+              <div className='board-detail-delete-button'>{'삭제'}</div>
+            </div>
+          </div>
+        </div>
+        <div className='divider'></div>
+        <div className='board-detail-top-main'>
+          <div className='board-detail-main-text'>{'되는 것인가'}</div>
+          <div className='board-detail-main-image'></div>
+        </div>
+      </div>
+    )
+
+  }
+
+  //       component: 게시물 상세 하단 컴포넌트           //
+  const BoardDetailBottom = () =>{
+
+    //        render : 게시물 상세 화면 컴포넌트 렌더링      //
+    return (<></>)
+  }
+
   //        render : 게시물 상세 화면 컴포넌트 렌더링      //
   return (
-    <div>게시물 상세 보기</div>
+    <div id='board-detail-wrapper'>
+      <div className='board-detail-container'>
+        <BoardDetailTop />
+        <BoardDetailBottom />
+      </div>
+    </div>
   )
 }
