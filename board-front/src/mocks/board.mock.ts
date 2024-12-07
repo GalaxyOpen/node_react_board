f42d4577c4e2e8fe95b2d7b993bbfd3017399ad5
DA317 <keehong11@naver.com>
2024-12-07 14:10:22 +0900

    24.12.07 게시물 상세 페이지 이벤트 처리 board.bock.ts 파일 생성 간단한 게시글 표시

---
+++ board-front/src/mocks/board.mock.ts
@@ -0,0 +1,14 @@
+import { Board } from "types/interface";
+
+const boardMock: Board = {
+    boardNumber: 1,
+    title: '동해물과 백두산이 마르고 닳도록',
+    content: '동해물과 백두산이 마르고 닳도록',
+    boardImageList: ['https://img.danawa.com/cp_images/service/139/2907210/k150016p1n17.jpg', 'https://file3.instiz.net/data/file3/2018/03/02/0/9/2/09263434bbfbc6088df41747f5b0a902.jpg'],
+    writeDateTime: '2024.12.07', 
+    writerEmail: 'email3@email.com',
+    writerNickname: '자바맨',
+    writerProifileImage: 'https://file3.instiz.net/data/file3/2018/03/02/0/9/2/09263434bbfbc6088df41747f5b0a902.jpg',
+}
+
+export default boardMock;
