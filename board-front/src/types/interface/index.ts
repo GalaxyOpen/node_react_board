1dc987f2932426f5488fddd8a04ca2156d592f07
DA317 <keehong11@naver.com>
2024-12-07 14:14:10 +0900

    24.12.07 게시물 상세 페이지 이벤트 처리 types > interface > Board.interface.ts :Mock 데이터 타입 추가

--- board-front/src/types/interface/index.ts
+++ board-front/src/types/interface/index.ts
@@ -1,10 +1,12 @@
 import User from './user.interface';
+import Board from './Board.interface';
 import BoardListItem from './board-list-item.interface';
 import FavoriteListItem from './favorite-list-item.inferface';
 import CommentListItem from './comment-list-item.interface';
 
 export type {
     User,
+    Board,
     BoardListItem,
     FavoriteListItem,
     CommentListItem
