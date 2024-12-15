464b3c0e4bf8fe0c393fc21cb8b5f53c3850c36d
DA317 <keehong11@naver.com>
2024-12-15 11:09:44 +0900

    24.12.15 게시물 상세 페이지 좋아요, 댓글 작성, 게시물 삭제 API 연동
    PutFavoriteDTO 생성 및 적용

--- board-front/src/apis/response/board/index.ts
+++ board-front/src/apis/response/board/index.ts
@@ -1,5 +1,6 @@
 import GetBoardResponseDTO from './get-board.response.DTO';
 import PostBoardResponseDTO from './post-board.response.DTO';
+import PutFavoriteResponseDTO from './put-favorite.response.DTO';
 import GetCommentListResponseDTO from './get-comment-list-response.dto';
 import GetFavoriteListResponseDTO from './get-favorite-list.response.dto';
 import IncresaeViewCountResponseDTO from './increase-view-count.response.dto';
@@ -7,6 +8,7 @@ import IncresaeViewCountResponseDTO from './increase-view-count.response.dto';
 export type {
     GetBoardResponseDTO,
     PostBoardResponseDTO,
+    PutFavoriteResponseDTO,
     GetCommentListResponseDTO,
     GetFavoriteListResponseDTO,
     IncresaeViewCountResponseDTO
