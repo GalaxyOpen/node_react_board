e5e57f81e2e556210061502b805150eb2bf7b851
DA317 <keehong11@naver.com>
2024-12-15 15:54:23 +0900

    24.12.15 게시물 상세 페이지 좋아요, 댓글 작성, 게시물 삭제 API 연동 apis >board > index.ts 에 DeleteBoardResponseDTO 추가.

--- board-front/src/apis/response/board/index.ts
+++ board-front/src/apis/response/board/index.ts
@@ -2,6 +2,7 @@ import GetBoardResponseDTO from './get-board.response.DTO';
 import PostBoardResponseDTO from './post-board.response.DTO';
 import PostCommentResponseDTO from './post-comment.response.DTO';
 import PutFavoriteResponseDTO from './put-favorite.response.DTO';
+import DeleteBoardResponseDTO from './delete-board.response.DTO';
 import GetCommentListResponseDTO from './get-comment-list-response.dto';
 import GetFavoriteListResponseDTO from './get-favorite-list.response.dto';
 import IncresaeViewCountResponseDTO from './increase-view-count.response.dto';
@@ -11,6 +12,7 @@ export type {
     PostBoardResponseDTO,
     PostCommentResponseDTO,
     PutFavoriteResponseDTO,
+    DeleteBoardResponseDTO,
     GetCommentListResponseDTO,
     GetFavoriteListResponseDTO,
     IncresaeViewCountResponseDTO
