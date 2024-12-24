c4e92800b27c7c7a7457d79ed968db14d243c268
DA317 <keehong11@naver.com>
2024-12-24 13:17:00 +0900

    24.12.24 게시물 수정 API 작성 오류가 아니라 해당 BackEnd 파일을 만들지 않은 것으로 사료되어 Backend 파일작성 BoardControllerDTO.java

--- board-back/src/main/java/com/lkh/board_back/controller/BoardController.java
+++ board-back/src/main/java/com/lkh/board_back/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lkh.board_back.dto.request.board.PatchBoardRequestDTO;
 import com.lkh.board_back.dto.request.board.PostBoardRequestDTO;
 import com.lkh.board_back.dto.request.board.PostCommentRequestDTO;
 import com.lkh.board_back.dto.response.board.DeleteBoardResponseDTO;
@@ -20,6 +22,7 @@ import com.lkh.board_back.dto.response.board.GetBoardResponseDTO;
 import com.lkh.board_back.dto.response.board.GetCommentListResponseDTO;
 import com.lkh.board_back.dto.response.board.GetFavoriteListResponseDTO;
 import com.lkh.board_back.dto.response.board.IncreaseViewCountResponseDTO;
+import com.lkh.board_back.dto.response.board.PatchBoardResponseDTO;
 import com.lkh.board_back.dto.response.board.PostBoardResponseDTO;
 import com.lkh.board_back.dto.response.board.PostCommentResponseDTO;
 import com.lkh.board_back.dto.response.board.PutFavoriteResponseDTO;
@@ -94,6 +97,16 @@ public class BoardController {
         return response;
     }
 
+    @PatchMapping("/{boardNumber}")
+    public ResponseEntity<? super PatchBoardResponseDTO> patchBoard(
+        @RequestBody @Valid PatchBoardRequestDTO requestBody,
+        @PathVariable("boardNumber") Integer boardNumber,
+        @AuthenticationPrincipal String email
+    ) {
+        ResponseEntity<? super PatchBoardResponseDTO> response = boardService.patchBoard(requestBody, boardNumber, email);
+        return response;
+    }
+
     @DeleteMapping("/{boardNumber}")
     public ResponseEntity<? super DeleteBoardResponseDTO> deleteBoard(
         @PathVariable("boardNumber") Integer boardNumber,
@@ -103,5 +116,3 @@ public class BoardController {
         return response;
     }
 }
-
-
