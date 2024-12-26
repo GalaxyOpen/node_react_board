52ca0c4efabded326168d16cc7998b48073bf35a
DA317 <keehong11@naver.com>
2024-12-26 16:10:04 +0900

    24.12.26 게시물 수정 페이지 API 연동 PatchBoardRequestDTO 추가하기

--- board-front/src/apis/request/board/index.ts
+++ board-front/src/apis/request/board/index.ts
@@ -1,6 +1,8 @@
 import PostBoardRequestDTO from './post-board.request.dto';
+import PatchBoardRequestDTO from './patch-board.request.dto';
 import PostCommentRequestDTO from './post-board-comment.request.dto';
 export type{
     PostBoardRequestDTO,
+    PatchBoardRequestDTO,
     PostCommentRequestDTO
 }
