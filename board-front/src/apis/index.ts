270c6561135904613edf73f0fe30f0f2a89eb364
DA317 <keehong11@naver.com>
2024-12-26 16:11:36 +0900

    24.12.26 게시물 수정 페이지 API 연동 추가한 response와 request 를 가지고 API 만들기

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -3,8 +3,8 @@ import { SignInRequestDTO, SignUpRequestDTO } from './request/auth';
 import { SignInResponseDTO, SignUpResponseDTO } from './response/auth';
 import { ResponseDTO } from './response';
 import { GetSignInUserResponseDTO } from './response/user';
-import { PostBoardRequestDTO, PostCommentRequestDTO } from './request/board';
-import { PostBoardResponseDTO, GetBoardResponseDTO, IncresaeViewCountResponseDTO, GetFavoriteListResponseDTO, GetCommentListResponseDTO, PutFavoriteResponseDTO, DeleteBoardResponseDTO } from './response/board';
+import { PatchBoardRequestDTO, PostBoardRequestDTO, PostCommentRequestDTO } from './request/board';
+import { PostBoardResponseDTO, GetBoardResponseDTO, IncresaeViewCountResponseDTO, GetFavoriteListResponseDTO, GetCommentListResponseDTO, PutFavoriteResponseDTO, DeleteBoardResponseDTO, PatchBoardResponseDTO } from './response/board';
 
 const DOMAIN = 'http://localhost:4000';
 const API_DOMAIN = `${DOMAIN}/api/v1`;
@@ -51,6 +51,7 @@ const GET_FAVORITE_LIST_URL = (boardNumber : number | string) => `${API_DOMAIN}/
 const GET_COMMENT_LIST_URL = (boardNumber : number | string) => `${API_DOMAIN}/board/${boardNumber}/comment-list`;
 const POST_BOARD_URL = () =>`${API_DOMAIN}/board`;
 const POST_COMMENT_URL = (boardNumber : number | string) => `${API_DOMAIN}/board/${boardNumber}/comment`;
+const PATCH_BOARD_URL = (boardNumber : number | string) => `${API_DOMAIN}/board/${boardNumber}`;
 const PUT_FAVORITE_URL = (boardNumber : number | string) => `${API_DOMAIN}/board/${boardNumber}/favorite`;
 const DELETE_BOARD_URL = (boardNumber : number | string) => `${API_DOMAIN}/board/${boardNumber}`;
 // 게시물 상세 페이지 특정 게시물 불러오기 API 연동
@@ -136,6 +137,20 @@ export const postCommentRequest = async (boardNumber : number | string, requestB
             return responseBody;
         })
     return result;
+}
+
+export const patchBoardRequest = async (boardNumber: number | string, requestBody: PatchBoardRequestDTO, accessToken:string) => {
+    const result = await axios.patch(PATCH_BOARD_URL(boardNumber), requestBody, authorization(accessToken))
+        .then(response =>{
+            const responseBody: PatchBoardResponseDTO = response.data;
+            return responseBody;
+        })
+        .catch(error =>{
+            if(!error.response) return null;
+            const responseBody: ResponseDTO =error.response.data;
+            return responseBody;
+        })
+    return result;    
 }
 
 export const putFavoriteRequest = async (boardNumber : number | string, accessToken: string) =>{
