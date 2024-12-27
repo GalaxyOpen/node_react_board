4c973585ffeee076a14525bdd70e3c3cb1194cc9
DA317 <keehong11@naver.com>
2024-12-27 15:59:44 +0900

    24.12.27 인기 검색어 리스트 불러오기 API 작성 URL : GET /api/v1/search/popular-list DTO 구성 전에 만든 ResultSet에 있는 getSearchWord( ) 를 가지고 인기 검색어를 가져옴.

---
+++ board-back/src/main/java/com/lkh/board_back/dto/response/search/GetPopularListResponseDTO.java
@@ -0,0 +1,38 @@
+package com.lkh.board_back.dto.response.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.lkh.board_back.common.ResponseCode;
+import com.lkh.board_back.common.ResponseMessage;
+import com.lkh.board_back.dto.response.ResponseDTO;
+import com.lkh.board_back.repository.resultSet.GetPopularListResultSet;
+
+import lombok.Getter;
+
+@Getter
+public class GetPopularListResponseDTO extends ResponseDTO {
+    
+    private List<String> popularWordList;
+
+    private GetPopularListResponseDTO (List<GetPopularListResultSet> resultSets) {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+
+        List<String> popularWordList = new ArrayList<>();
+
+        for(GetPopularListResultSet resultSet:resultSets){
+            String popularWord = resultSet.getSearchWord();
+            popularWordList.add(popularWord);
+        } 
+
+        this.popularWordList = popularWordList;
+    }
+
+    public static ResponseEntity<GetPopularListResponseDTO> success(List<GetPopularListResultSet> resultSets) {
+        GetPopularListResponseDTO result = new GetPopularListResponseDTO(resultSets);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
