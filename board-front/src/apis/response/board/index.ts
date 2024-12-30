a53030c24b32fe74c4cba6d7bdff97db286acb45
DA317 <keehong11@naver.com>
2024-12-30 14:23:38 +0900

    24.12.30 메인 페이지 API 연동 최신 게시물 latestList 반환 만들기 get-latest-board-list.response.DTO.ts 생성 dto -> DTO 파일로 모두 변환

--- board-front/src/apis/response/board/index.ts
+++ board-front/src/apis/response/board/index.ts
@@ -2,12 +2,13 @@ import GetBoardResponseDTO from './get-board.response.DTO';
 import PostBoardResponseDTO from './post-board.response.DTO';
 import PostCommentResponseDTO from './post-comment.response.DTO';
 import PutFavoriteResponseDTO from './put-favorite.response.DTO';
-import PatchBoardResponseDTO from './patch-board.response.dto';
+import PatchBoardResponseDTO from './patch-board.response.DTO';
 import DeleteBoardResponseDTO from './delete-board.response.DTO';
-import GetCommentListResponseDTO from './get-comment-list-response.dto';
-import GetFavoriteListResponseDTO from './get-favorite-list.response.dto';
-import IncresaeViewCountResponseDTO from './increase-view-count.response.dto';
+import GetCommentListResponseDTO from './get-comment-list-response.DTO';
+import GetFavoriteListResponseDTO from './get-favorite-list.response.DTO';
+import IncresaeViewCountResponseDTO from './increase-view-count.response.DTO';
 import GetTop3BoardListResponseDTO from './get-top-3-board-list.response.DTO';
+import GetLatestBoardListResponseDTO from './get-latest-board-list.response.DTO';
 
 export type {
     GetBoardResponseDTO,
@@ -19,5 +20,6 @@ export type {
     GetCommentListResponseDTO,
     GetFavoriteListResponseDTO,
     GetTop3BoardListResponseDTO,
-    IncresaeViewCountResponseDTO
+    IncresaeViewCountResponseDTO,
+    GetLatestBoardListResponseDTO
 }
