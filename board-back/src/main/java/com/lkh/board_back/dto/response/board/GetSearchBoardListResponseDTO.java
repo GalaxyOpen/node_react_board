311c67a91f65889834f31d43a71c14982fa8d4d1
DA317 <keehong11@naver.com>
2024-12-31 14:46:31 +0900

    24.12.31 검색 게시물 리스트 불러오기 API 작성 [EndPoint가 2개] GET : /api/v1/board/search-list/{searchWord} GET : /api/v1/board/search-list/{searchWord}/{preSearchWord} GetSearchBoardListResponseDTO.java 생성

---
+++ board-back/src/main/java/com/lkh/board_back/dto/response/board/GetSearchBoardListResponseDTO.java
@@ -0,0 +1,30 @@
+package com.lkh.board_back.dto.response.board;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.lkh.board_back.common.ResponseCode;
+import com.lkh.board_back.common.ResponseMessage;
+import com.lkh.board_back.dto.object.BoardListItem;
+import com.lkh.board_back.dto.response.ResponseDTO;
+import com.lkh.board_back.entity.BoardListViewEntity;
+
+import lombok.Getter;
+
+@Getter
+public class GetSearchBoardListResponseDTO extends ResponseDTO {
+    
+    private List<BoardListItem> searchList; 
+
+    private GetSearchBoardListResponseDTO(List<BoardListViewEntity> boardListViewEntities) {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.searchList = BoardListItem.getList(boardListViewEntities);
+    }
+
+    public ResponseEntity<GetSearchBoardListResponseDTO> success(List<BoardListViewEntity> boardListViewEntities) {
+        GetSearchBoardListResponseDTO result = new GetSearchBoardListResponseDTO(boardListViewEntities);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
