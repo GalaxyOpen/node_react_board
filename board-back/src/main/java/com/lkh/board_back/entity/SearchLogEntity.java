419b93e4f3eaf9323345ba6e5f4fd861522d6753
DA317 <keehong11@naver.com>
2024-12-31 15:41:31 +0900

    24.12.31 검색 게시물 리스트 불러오기 API 작성 [EndPoint가 2개] GET : /api/v1/board/search-list/{searchWord} GET : /api/v1/board/search-list/{searchWord}/{preSearchWord} SearchLogEntity에 생성자 형성.

--- board-back/src/main/java/com/lkh/board_back/entity/SearchLogEntity.java
+++ board-back/src/main/java/com/lkh/board_back/entity/SearchLogEntity.java
@@ -16,9 +16,16 @@ import lombok.NoArgsConstructor;
 @Entity(name="search_log")
 @Table(name="search_log")
 public class SearchLogEntity {
+    
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int sequence;
-    private String seachWord;
+    private String searchWord;
     private String relationWord;
     private boolean relation;
+
+    public SearchLogEntity(String searchWord, String relationWord, boolean relation) {
+        this.searchWord = searchWord;
+        this.relationWord = relationWord;
+        this.relation = relation ;
+    }
 }
