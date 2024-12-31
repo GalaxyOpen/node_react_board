5e592341f1ac771f3b780e3811facc8f41a5450d
DA317 <keehong11@naver.com>
2024-12-31 16:19:20 +0900

    24.12.31 연관 검색어 리스트 불러오기 API 작성 GET : /api/v1/search/{searchWord}/relation-list DTO 작성

--- board-back/src/main/java/com/lkh/board_back/dto/response/search/GetRelationListResponseDTO.java
+++ board-back/src/main/java/com/lkh/board_back/dto/response/search/GetRelationListResponseDTO.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import com.lkh.board_back.common.ResponseCode;
 import com.lkh.board_back.common.ResponseMessage;
 import com.lkh.board_back.dto.response.ResponseDTO;
+import com.lkh.board_back.repository.resultSet.GetRelationListResultSet;
 
 import lombok.Getter;
 
@@ -17,17 +18,20 @@ public class GetRelationListResponseDTO extends ResponseDTO {
     
     private List<String> relativeWordList;
 
-    private GetRelationListResponseDTO() {
+    private GetRelationListResponseDTO(List<GetRelationListResultSet> resultSets) {
         super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
         
         List<String> relativeWordList = new ArrayList<>();
-        
+        for(GetRelationListResultSet resultSet : resultSets) {
+            String relativeWord = resultSet.getSearchWord();
+            relativeWordList.add(relativeWord);
+        }
 
         this.relativeWordList = relativeWordList;
     }
 
-    public static ResponseEntity<GetRelationListResponseDTO> success() {
-        GetRelationListResponseDTO result = new GetRelationListResponseDTO();
+    public static ResponseEntity<GetRelationListResponseDTO> success(List<GetRelationListResultSet> resultSets) {
+        GetRelationListResponseDTO result = new GetRelationListResponseDTO(resultSets);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
