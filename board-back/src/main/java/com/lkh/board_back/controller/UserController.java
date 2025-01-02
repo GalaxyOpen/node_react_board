00bf3de05731420945de58a45fb1c1b8b43c7aac
DA317 <keehong11@naver.com>
2025-01-02 16:42:16 +0900

    24.01.02 유저 및 유저 게시물 불러오기 API 작성 1. 유저 정보 불러오기 1-3. 컨트롤러 작성 이름에 i가 들어가있어 그것도 제거. getUser 메서드 완료하기

--- board-back/src/main/java/com/lkh/board_back/controller/UserController.java
+++ board-back/src/main/java/com/lkh/board_back/controller/UserController.java
@@ -3,8 +3,10 @@ package com.lkh.board_back.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.lkh.board_back.dto.response.user.GetSignInUserResponseDTO;
+import com.lkh.board_back.dto.response.user.GetUserResponseDTO;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lkh.board_back.service.UserService;
@@ -14,10 +16,17 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
-public class UserControiller {
+public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/{email}")
+    public ResponseEntity<? super GetUserResponseDTO> getUser(
+        @PathVariable("email") String email
+    ){
+        ResponseEntity<? super GetUserResponseDTO> response = userService.getUser(email);
+        return response;
+    }
     @GetMapping("")
     public ResponseEntity<? super GetSignInUserResponseDTO> getSignInUser(
         @AuthenticationPrincipal String email
