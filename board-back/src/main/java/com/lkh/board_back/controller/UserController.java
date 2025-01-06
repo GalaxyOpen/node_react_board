97fa825ba361367c90695124c76dcd2b51c07602
DA317 <keehong11@naver.com>
2025-01-06 14:36:38 +0900

    24.01.06 닉네임 및 프로필 이미지 수정 API 작성 URL : 1) 닉네임 수정 PATCH /api/v1/user/nickname 2) 프로필 이미지 수정 PATCH /api/vi/user/profile-image
    UserController에 매핑한다.

--- board-back/src/main/java/com/lkh/board_back/controller/UserController.java
+++ board-back/src/main/java/com/lkh/board_back/controller/UserController.java
@@ -1,12 +1,22 @@
 package com.lkh.board_back.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lkh.board_back.dto.request.user.PatchNicknameRequestDTO;
+import com.lkh.board_back.dto.request.user.PatchProfileImageRequestDTO;
 import com.lkh.board_back.dto.response.user.GetSignInUserResponseDTO;
 import com.lkh.board_back.dto.response.user.GetUserResponseDTO;
+import com.lkh.board_back.dto.response.user.PatchNicknameResponseDTO;
+import com.lkh.board_back.dto.response.user.PatchProfileImageResponseDTO;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lkh.board_back.service.UserService;
@@ -27,6 +37,7 @@ public class UserController {
         ResponseEntity<? super GetUserResponseDTO> response = userService.getUser(email);
         return response;
     }
+
     @GetMapping("")
     public ResponseEntity<? super GetSignInUserResponseDTO> getSignInUser(
         @AuthenticationPrincipal String email
@@ -35,4 +46,21 @@ public class UserController {
         return response;
     }
     
+    @PatchMapping("/nickname")
+    public ResponseEntity<? super PatchNicknameResponseDTO> patchNickname (
+        @RequestBody @Valid PatchNicknameRequestDTO requestBody,
+        @AuthenticationPrincipal String email
+    ) {
+        ResponseEntity<? super PatchNicknameResponseDTO> response = userService.patchNickname(requestBody, email);
+        return response;
+    }
+
+    @PatchMapping("/profile-image")
+    public ResponseEntity<? super PatchProfileImageResponseDTO> patchProfileImage(
+        @RequestBody @Valid PatchProfileImageRequestDTO requestBody,
+        @AuthenticationPrincipal String email
+    ) {
+        ResponseEntity<? super PatchProfileImageResponseDTO> response = userService.patchProfileImage(requestBody, email);
+        return response;
+    }
 }
