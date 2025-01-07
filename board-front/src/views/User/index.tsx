97e3e2cc6e652e5a8a7d831057e07a566dcd250e
DA317 <keehong11@naver.com>
2025-01-07 16:04:33 +0900

    25.01.07 유저 페이지 상단 컴포넌트 스타일 작성 및 이벤트 처리 2. 이벤트 처리  - 프로필 박스 버튼 클릭 이벤트  - 닉네임 수정 버튼 클릭 이벤트  - 프로필 이미지 변경 이벤트  - 닉네임 변경 이벤트

--- board-front/src/views/User/index.tsx
+++ board-front/src/views/User/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import './style.css'
 import defaultProfileImage from 'assets/image/default-profile-image.png';
 import { useParams } from 'react-router-dom';
@@ -25,19 +25,47 @@ export default function UserPage() {
     //        state :프로필 이미지 상태        //
     const [profileImage, setProfileImage] = useState<string  | null>(null);
 
+    //        event handler : 프로필 박스 버튼 클릭 이벤트 처리         //
+    const onProfileBoxClickHandler = () =>{
+      if(!isMyPage) return;
+      if (!imageInputRef.current) return;
+      imageInputRef.current.click();
+    };
+
+    //        event handler : 닉네임 수정 버튼 클릭 이벤트 처리         //
+    const onNickNameEditButtonClickHandler = () =>{
+      setChangeNickname(nickname);
+      setNicknameChange(!isNicknameChange);
+    };
+
+    //        event handler : 프로필 이미지 변경 이벤트 처리         //
+    const onProfileImageChangeHandler = (event: ChangeEvent<HTMLInputElement>) =>{
+      if(!event.target.files || !event.target.files?.length) return;
+
+      const file = event.target.files[0];
+      const data = new FormData();
+      data.append('file',file);
+    };
+
+    //        event Handler : 닉네임 변경 이벤트 처리         //
+    const onNicknameChangeHandler = (event: ChangeEvent<HTMLInputElement>) =>{
+      const { value } = event.target;
+      setChangeNickname(value);
+    };
+
     //        Effect : user email path variable 변경 시 실행할 함수         //
     useEffect(()=>{
       if(!userEmail) return;
       setNickname('I\'m pythonman');
       setProfileImage('https://blog.kakaocdn.net/dn/bbTLnu/btshry66BH2/B4kENPa6CvSZNy0OgC7SQk/img.jpg');
-    },[userEmail])
+    },[userEmail]);
 
     //        render : 유저 화면 상단 컴포넌트        //
     return (
       <div id='user-top-wrapper'>
         <div className='user-top-container'>
           {isMyPage ?
-          <div className='user-top-my-profile-image-box'>
+          <div className='user-top-my-profile-image-box' onClick={onProfileBoxClickHandler}>
             {profileImage !== null ?
             <div className='user-top-profile-image' style={{backgroundImage: `url(${profileImage})`}}></div> :
             <div className='user-top-my-profile-image-nothing-box'>
@@ -47,19 +75,19 @@ export default function UserPage() {
             </div> 
             }
             
-            <input ref={imageInputRef} type="file" accept='image/*' style={ {display: 'none'}} />
+            <input ref={imageInputRef} type="file" accept='image/*' style={ {display: 'none'}} onChange={onProfileImageChangeHandler} />
           </div> :
-          <div className='user-top-profile-image' style={{backgroundImage: `url(${profileImage ? profileImage : defaultProfileImage})`}}></div>
+          <div className='user-top-profile-image-box' style={{backgroundImage: `url(${profileImage ? profileImage : defaultProfileImage})`}}></div>
           }
           <div className='user-top-info-box'>
             <div className='user-top-info-nickname-box'>
               {isMyPage ? 
               <>
               {isNicknameChange ? 
-              <input className='user-top-info-nickname-input' type="text" size={nickname.length + 1} value={changeNickname} /> : 
+              <input className='user-top-info-nickname-input' type="text" size={nickname.length + 2} value={changeNickname} onChange={onNicknameChangeHandler} /> : 
               <div className='user-top-info-nickname'>{nickname}</div>    
               }              
-              <div className='icon-button'>
+              <div className='icon-button' onClick={onNickNameEditButtonClickHandler}>
                 <div className='icon edit-icon'></div>
               </div>
               </> :
