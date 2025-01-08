b0f61cce32b81edf63358ea60c72577bf2eac3f9
DA317 <keehong11@naver.com>
2025-01-08 16:01:23 +0900

    25.01.08 유저 페이지 API 연동 7. apis > request  > user > index.ts 생성 PatchNicknameRequestDTO, PatchProfileImageRequestDTO 넣기

---
+++ board-front/src/apis/request/user/index.ts
@@ -0,0 +1,8 @@
+import PatchNicknameRequestDTO from "./patch-nickname.request.DTO";
+import PatchProfileImageRequestDTO from "./patch-profile-image.request.DTO";
+
+
+export type {
+    PatchNicknameRequestDTO,
+    PatchProfileImageRequestDTO
+}
