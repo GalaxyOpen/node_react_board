e3d2babb27e3eae76124272ea4da6f781d33dd57
DA317 <keehong11@naver.com>
2025-01-08 16:07:23 +0900

    25.01.08 유저 페이지 API 연동 10. apis > index.ts 1. GET_USER_BOARD_LIST_URL 생성 2. getUserBoardListRequest 함수 생성 3. GET_USER_URL 생성 4. getUserRequest 함수 생성 5. PATCH_NICKNAME_URL 생성 6. patchNicknameRequest 함수 생성 7. PATCH_PROFILE_IMAGE_URL 생성 8. patchProfileImageRequest 함수 생성

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -2,18 +2,19 @@ import axios from 'axios';
 import { SignInRequestDTO, SignUpRequestDTO } from './request/auth';
 import { SignInResponseDTO, SignUpResponseDTO } from './response/auth';
 import { ResponseDTO } from './response';
-import { GetSignInUserResponseDTO } from './response/user';
+import { GetSignInUserResponseDTO, GetUserResponseDTO, PatchNicknameResponseDTO, PatchProfileImageResponseDTO } from './response/user';
 import { PatchBoardRequestDTO, PostBoardRequestDTO, PostCommentRequestDTO } from './request/board';
-import { PostBoardResponseDTO, GetBoardResponseDTO, IncresaeViewCountResponseDTO, GetFavoriteListResponseDTO, GetCommentListResponseDTO, PutFavoriteResponseDTO, DeleteBoardResponseDTO, PatchBoardResponseDTO, GetLatestBoardListResponseDTO, GetTop3BoardListResponseDTO, GetSearchBoardListResponseDTO } from './response/board';
+import { PostBoardResponseDTO, GetBoardResponseDTO, IncresaeViewCountResponseDTO, GetFavoriteListResponseDTO, GetCommentListResponseDTO, PutFavoriteResponseDTO, DeleteBoardResponseDTO, PatchBoardResponseDTO, GetLatestBoardListResponseDTO, GetTop3BoardListResponseDTO, GetSearchBoardListResponseDTO, GetUserBoardListResponseDTO } from './response/board';
 import { GetPopularListResponseDTO, GetRelationListResponseDTO } from './response/search';
 import { response } from 'express';
+import { PatchNicknameRequestDTO, PatchProfileImageRequestDTO } from './request/user';
 
 const DOMAIN = 'http://localhost:4000';
 const API_DOMAIN = `${DOMAIN}/api/v1`;
 
 const authorization = (accessToken:string) => {
     return {headers:{Authorization:`Bearer ${accessToken}`}}
-}
+};
 
 const SIGN_IN_URL = () => `${API_DOMAIN}/auth/sign-in`;
 const SIGN_UP_URL = () => `${API_DOMAIN}/auth/sign-up`;
@@ -31,7 +32,7 @@ export const signInRequest = async (requestBody: SignInRequestDTO) => {
         });
         console.log('Request Body: ', requestBody);
     return result;    
-}
+};
 
 export const signUpRequest = async (requestBody: SignUpRequestDTO) =>{
     const result = await axios.post(SIGN_UP_URL(), requestBody)
@@ -45,12 +46,13 @@ export const signUpRequest = async (requestBody: SignUpRequestDTO) =>{
             return responseBody;
         });
     return result;
-}
+};
 
 const GET_BOARD_URL = (boardNumber : number | string) => `${API_DOMAIN}/board/${boardNumber}`; // 게시물 상세 페이지 특정 게시물 불러오기 API 연동
 const GET_LATEST_BOARD_LIST_URL = () => `${API_DOMAIN}/board/latest-list`;
 const GET_TOP_3_BOARD_LIST_URL = () => `${API_DOMAIN}/board/top-3`;
 const GET_SEARCH_BOARD_LIST_URL=(searchWord : string, preSearchWord: string | null )=> `${API_DOMAIN}/board/search-list/${searchWord}${preSearchWord ? '/' + preSearchWord : ''}`;
+const GET_USER_BOARD_LIST_URL = (email: string) => `${API_DOMAIN}/board/user-board-list/${email}`;
 const INCREASE_VIEW_COUNT_URL = (boardNumber: number | string ) => `${API_DOMAIN}/board/${boardNumber}/increase-view-count`; // 조회 수 4개 증가 방지용 API 
 const GET_FAVORITE_LIST_URL = (boardNumber : number | string) => `${API_DOMAIN}/board/${boardNumber}/favorite-list`;
 const GET_COMMENT_LIST_URL = (boardNumber : number | string) => `${API_DOMAIN}/board/${boardNumber}/comment-list`;
@@ -100,7 +102,7 @@ export const getTop3BoardListRequest = async () => {
             return responseBody;
         });
     return result;
-}
+};
 
 export const getSearchBoardListRequest = async (searchWord:string, preSearchWord : string | null) => {
     const result = await axios.get(GET_SEARCH_BOARD_LIST_URL(searchWord, preSearchWord))
@@ -114,7 +116,21 @@ export const getSearchBoardListRequest = async (searchWord:string, preSearchWord
             return responseBody;
         });
     return result;    
-}
+};
+
+export const getUserBoardListRequest = async (email : string) => {
+    const result = await axios.get(GET_USER_BOARD_LIST_URL(email))
+        .then(response =>{
+            const responseBody: GetUserBoardListResponseDTO = response.data;
+            return responseBody;
+        })
+        .catch(error =>{
+            if(!error.response) return null;
+            const responseBody : ResponseDTO = error.resopnse.data;
+            return responseBody;
+        });
+    return result;    
+};
 
 export const increaseViewCountRequest = async (boardNumber: number | string) =>{
     const result = await axios.get(INCREASE_VIEW_COUNT_URL(boardNumber))
@@ -128,7 +144,7 @@ export const increaseViewCountRequest = async (boardNumber: number | string) =>{
             return responseBody;            
         });
     return result;    
-}
+};
 
 export const getFavoriteListRequest = async (boardNumber: number | string) =>{
     const result = await axios.get(GET_FAVORITE_LIST_URL(boardNumber))
@@ -142,7 +158,7 @@ export const getFavoriteListRequest = async (boardNumber: number | string) =>{
             return responseBody;
         });
     return result;
-}   
+};
 
 export const GetCommentListRequest = async (boardNumber:number | string) => {
     const result = await axios.get(GET_COMMENT_LIST_URL(boardNumber))
@@ -156,7 +172,7 @@ export const GetCommentListRequest = async (boardNumber:number | string) => {
             return responseBody;
         });
     return result;    
-}
+};
 
 export const postBoardRequest = async (requestBody: PostBoardRequestDTO, accessToken:string)=>{
     const result = await axios.post(POST_BOARD_URL(), requestBody, authorization(accessToken))
@@ -170,7 +186,7 @@ export const postBoardRequest = async (requestBody: PostBoardRequestDTO, accessT
             return responseBody;
         });
     return result;    
-}
+};
 
 export const postCommentRequest = async (boardNumber : number | string, requestBody: PostCommentRequestDTO, accessToken: string) =>{
     const result = await axios.post(POST_COMMENT_URL(boardNumber), requestBody, authorization(accessToken))
@@ -184,7 +200,7 @@ export const postCommentRequest = async (boardNumber : number | string, requestB
             return responseBody;
         });
     return result;
-}
+};
 
 export const patchBoardRequest = async (boardNumber: number | string, requestBody: PatchBoardRequestDTO, accessToken:string) => {
     const result = await axios.patch(PATCH_BOARD_URL(boardNumber), requestBody, authorization(accessToken))
@@ -198,7 +214,7 @@ export const patchBoardRequest = async (boardNumber: number | string, requestBod
             return responseBody;
         });
     return result;    
-}
+};
 
 export const putFavoriteRequest = async (boardNumber : number | string, accessToken: string) =>{
     const result = await axios.put(PUT_FAVORITE_URL(boardNumber),{}, authorization(accessToken))
@@ -212,7 +228,7 @@ export const putFavoriteRequest = async (boardNumber : number | string, accessTo
             return responseBody;
         });
     return result;    
-}
+};
 
 export const deleteBoardRequest = async(boardNumber : number | string, accessToken : string ) =>{
     const result = await axios.delete(DELETE_BOARD_URL(boardNumber), authorization(accessToken))
@@ -226,7 +242,7 @@ export const deleteBoardRequest = async(boardNumber : number | string, accessTok
             return responseBody;
         });
     return result;
-}
+};
 
 const GET_POPULAR_LIST_URL = () => `${API_DOMAIN}/search/popular-list`;
 const GET_RELATION_LIST_URL = (searchWord: string) => `${API_DOMAIN}/search/${searchWord}/relation-list`;
@@ -241,9 +257,9 @@ export const getPopularListRequest = async () =>{
             if(!error.response) return null;
             const responseBody : ResponseDTO = error.response.data;
             return responseBody;
-        })
+        });
     return result;    
-}
+};
 
 export const getRelationListRequest = async (searchWord : string) =>{
     const result = await axios.get(GET_RELATION_LIST_URL(searchWord)) 
@@ -255,11 +271,28 @@ export const getRelationListRequest = async (searchWord : string) =>{
             if(!error.response) return null;
             const responseBody : ResponseDTO = error.response.data;
             return responseBody;
-        })
+        });
     return result;    
-}
+};
 
+const GET_USER_URL = (email : string) => `${API_DOMAIN}/user/${email}`; 
 const GET_SIGN_IN_USER_URL = () => `${API_DOMAIN}/user`;
+const PATCH_NICKNAME_URL = () => `${API_DOMAIN}/user/nickname`;
+const PATCH_PROFILE_IMAGE_URL = () =>`${API_DOMAIN}/user/profile-image`;
+
+export const getUserRequest = async (email:string) =>{
+    const result = await axios.get(GET_USER_URL(email))
+        .then(response =>{
+            const responseBody: GetUserResponseDTO = response.data;
+            return responseBody;
+        })
+        .catch(error =>{
+            if(!error.resopnse) return null;
+            const responseBody = error.response.data;
+            return responseBody;
+        });
+    return result;    
+};
 
 export const getSignInUserRequest = async (accessToken: string) =>{
     const result = await axios.get(GET_SIGN_IN_USER_URL(), authorization(accessToken))
@@ -273,7 +306,35 @@ export const getSignInUserRequest = async (accessToken: string) =>{
             return responseBody;
         });
     return result;    
+};
+
+export const patchNicknameRequest = async (requestBody : PatchNicknameRequestDTO, accessToken : string) =>{
+    const result = await axios.patch(PATCH_NICKNAME_URL(), requestBody, authorization(accessToken))
+        .then(response=>{
+            const responseBody:PatchNicknameResponseDTO = response.data;
+            return responseBody;
+        })
+        .catch(error=>{
+            if(!error.response) return null;
+            const responseBody: ResponseDTO =error.response.data;
+            return responseBody;
+        });
+    return result;    
+};
+
+export const patchProfileImageReqeust = async (requestBody:PatchProfileImageRequestDTO, accessToken:string) =>{
+    const result = await axios.patch(PATCH_PROFILE_IMAGE_URL(), requestBody, authorization(accessToken))
+        .then(response=>{
+            const responseBody : PatchProfileImageResponseDTO = response.data;
+            return responseBody;
+        }).catch(error=>{
+            if(!error.response) return null;
+            const responseBody : ResponseDTO = error.resopnse.data;
+            return responseBody;
+        });
+    return result;    
 }
+
 const FILE_DOMAIN = `${DOMAIN}/file`;
 
 const FILE_UPLOAD_URL = () => `${FILE_DOMAIN}/upload`;
@@ -290,4 +351,4 @@ export const fileUploadRequest = async (data:FormData) =>{
             return null;
         });
     return result;    
-}
+};
