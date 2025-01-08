2302238da81b9f29d73a88c38bb6add79d949906
DA317 <keehong11@naver.com>
2025-01-08 16:02:41 +0900

    25.01.08 유저 페이지 API 연동 8. apis > response  > user > index.ts 수정 GetUserResponseDTO, PatchNicknameResponseDTO, PatchProfileImageResponseDTO 넣기

--- board-front/src/apis/response/user/index.ts
+++ board-front/src/apis/response/user/index.ts
@@ -1,5 +1,12 @@
-import GetSignInUserResponseDTO from './get-sign-in-user.response.dto';
+import GetUserResponseDTO from './get-user.response.DTO';
+import GetSignInUserResponseDTO from './get-sign-in-user.response.DTO';
+import PatchNicknameResponseDTO from './patch-nickname.response.DTO';
+import PatchProfileImageResponseDTO from './patch-profile-image.response.DTO';
+
 
 export type{
-    GetSignInUserResponseDTO
+    GetUserResponseDTO,
+    GetSignInUserResponseDTO,    
+    PatchNicknameResponseDTO,
+    PatchProfileImageResponseDTO
 }
