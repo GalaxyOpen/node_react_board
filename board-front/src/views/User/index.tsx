b68c657da70605ca328fa687744978155008b253
DA317 <keehong11@naver.com>
2025-01-08 11:57:07 +0900

    25.01.08 유저 페이지 하단 컴포넌트 구조, 스타일 작성 및 이벤트 처리 1. 페이지 하단 컴포넌트 구조 짜기

--- board-front/src/views/User/index.tsx
+++ board-front/src/views/User/index.tsx
@@ -1,21 +1,31 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import './style.css'
 import defaultProfileImage from 'assets/image/default-profile-image.png';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { BoardListItem } from 'types/interface';
+import { latestBoardListMock } from 'mocks';
+import BoardItem from 'components/BoardItem';
+import { BOARD_PATH, BOARD_WRITE_PATH, MAIN_PATH, USER_PATH } from 'constant';
+import { useLoginUserStore } from 'stores';
 
 //       component: 유저 화면 컴포넌트           //
 export default function UserPage() {
 
-  //        state : userEmail 가져오기        //
+  //        state : userEmail path variable 상태        //
   const { userEmail } = useParams();
+  //        state : 로그인 유저 상태        //
+  const { loginUser } = useLoginUserStore();
+  //        state : 마이페이지 여부 상태        //
+  const [isMyPage, setMyPage] = useState<boolean>(false);
 
-//       component: 유저 화면 상단 컴포넌트           //
+  //        function : 내비게이트 함수         //
+  const navigate = useNavigate();
+
+  //       component: 유저 화면 상단 컴포넌트           //
   const UserTop = () => {
     
     //        state : 이미지 파일 인풋 참조 상태        //
     const imageInputRef = useRef<HTMLInputElement | null>(null);
-    //        state : 마이페이지 여부 상태        //
-    const [isMyPage, setMyPage] = useState<boolean>(true);
     //        state :닉네임 변경 여부 상태        //
     const [isNicknameChange, setNicknameChange] = useState<boolean>(false);
     //        state :닉네임 상태        //
@@ -41,7 +51,6 @@ export default function UserPage() {
     //        event handler : 프로필 이미지 변경 이벤트 처리         //
     const onProfileImageChangeHandler = (event: ChangeEvent<HTMLInputElement>) =>{
       if(!event.target.files || !event.target.files?.length) return;
-
       const file = event.target.files[0];
       const data = new FormData();
       data.append('file',file);
@@ -73,8 +82,7 @@ export default function UserPage() {
                 <div className='icon image-box-white-icon'></div>
               </div>
             </div> 
-            }
-            
+            }            
             <input ref={imageInputRef} type="file" accept='image/*' style={ {display: 'none'}} onChange={onProfileImageChangeHandler} />
           </div> :
           <div className='user-top-profile-image-box' style={{backgroundImage: `url(${profileImage ? profileImage : defaultProfileImage})`}}></div>
@@ -102,12 +110,61 @@ export default function UserPage() {
   };
 
   //       component: 유저 화면 하단 컴포넌트           //
-
   const UserBottom = () => {
+
+    //        state: 게시물 개수 상태         //
+    const [count, setCount] = useState<number>(2);
+    //        state: 게시물 리스트 상태 (임시)         //
+    const [userBoardList, setUserBoardList] = useState<BoardListItem[]>([]);
+
+    //        event handler: 사이드 카드 클릭 이벤트 처리          //
+    const onSideCardClickHandler = ()=>{
+      if(isMyPage) navigate(BOARD_PATH()+'/'+BOARD_WRITE_PATH());
+      else if(loginUser) navigate(USER_PATH(loginUser.email));
+    }
+
+    
+    //        Effect : user Email Path Variable 변경될 때마다 실행할 함수         //
+    useEffect(() =>{
+      setUserBoardList(latestBoardListMock);
+    }, [userEmail]);
 
     //        render : 유저 화면 하단 컴포넌트        //
     return (
-      <div></div>
+      <div id='user-bottom-wrapper'>
+        <div className='user-bottom-container'>
+          <div className='user-bottom-title'>{isMyPage ? ' 내 게시물 ' : ' 게시물 '} <span className='emphasis'>{count}</span> </div>
+          <div className='user-bottom-contents-box'>
+            {count === 0 ? 
+            <div className='user-bottom-contents-nothing'>{'게시물이 없습니다. '}</div> :
+            <div className='user-bottom-contents'>
+              {userBoardList.map(boardListItem => <BoardItem boardListItem={boardListItem} />)}
+            </div>
+            }
+            <div className='user-bottom-side-box'>
+              <div className='user-bottom-side-card' onClick={onSideCardClickHandler}>
+                <div className='user-bottom-side-container'>
+                  {isMyPage ?
+                  <>
+                  <div className='icon-box'>
+                    <div className='icon edit-icon'></div>
+                  </div>
+                  <div className='user-bottom-side-text'>{'글쓰기'}</div>
+                  </> :
+                  <>
+                  <div className='user-bottom-side-text'>{'내 게시물로 가기'}</div>
+                  <div className='icon-box'>
+                    <div className='icon arrow-right-icon'></div>
+                  </div>
+                  </>
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='user-bottom-pagination-box'></div>
+        </div>
+      </div>
     );
   };
 
