5d7b30360f6b35140d45224f4a570295ae187bbc
DA317 <keehong11@naver.com>
2025-01-08 16:03:33 +0900

    25.01.08 유저 페이지 API 연동 9. apis > response  > board > index.ts 수정 PatchBoardResponseDTO, GetUserBordListREsponseDTO 삽입

--- board-front/src/apis/response/board/index.ts
+++ board-front/src/apis/response/board/index.ts
@@ -1,26 +1,29 @@
 import GetBoardResponseDTO from './get-board.response.DTO';
 import PostBoardResponseDTO from './post-board.response.DTO';
+import PatchBoardResponseDTO from './patch-board.response.DTO';
 import PostCommentResponseDTO from './post-comment.response.DTO';
 import PutFavoriteResponseDTO from './put-favorite.response.DTO';
-import PatchBoardResponseDTO from './patch-board.response.DTO';
 import DeleteBoardResponseDTO from './delete-board.response.DTO';
 import GetCommentListResponseDTO from './get-comment-list-response.DTO';
 import GetFavoriteListResponseDTO from './get-favorite-list.response.DTO';
+import GetUserBoardListResponseDTO from './get-user-board-response.DTO';
 import IncresaeViewCountResponseDTO from './increase-view-count.response.DTO';
 import GetTop3BoardListResponseDTO from './get-top-3-board-list.response.DTO';
 import GetLatestBoardListResponseDTO from './get-latest-board-list.response.DTO';
-import GetSearchBoardListResponseDTO from './get-search-board-list.response.DTO'
+import GetSearchBoardListResponseDTO from './get-search-board-list.response.DTO';
+
 
 export type {
     GetBoardResponseDTO,
     PostBoardResponseDTO,
-    PostCommentResponseDTO,
     PatchBoardResponseDTO,
+    PostCommentResponseDTO,
     PutFavoriteResponseDTO,
     DeleteBoardResponseDTO,
     GetCommentListResponseDTO,
     GetFavoriteListResponseDTO,
     GetTop3BoardListResponseDTO,
+    GetUserBoardListResponseDTO,
     IncresaeViewCountResponseDTO,
     GetLatestBoardListResponseDTO,
     GetSearchBoardListResponseDTO
